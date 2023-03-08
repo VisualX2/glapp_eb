fbc16b37213bd895de2463e15c72570c9492ed71
VisualX2 <visualx321@gmail.com>
2023-03-08 14:28:46 +0200

    CutFace sides

--- src/components/XNCOperationComponents/CutFaceOperation.tsx
+++ src/components/XNCOperationComponents/CutFaceOperation.tsx
@@ -17,16 +17,16 @@ export const CutFaceOperation: React.FC<CutFaceParameters> = observer(({angle,si
 
     const bC = () => {
         if (side === "left") {
-            return <Group><Line points={[dist,0,dist,partheight]} stroke="red" dash={[5,2]}></Line><Line points={[dist, -5,0,-5 - partdepth]} stroke="red" dash={[5,2]}></Line><Line points={[dist, partheight + 5,0,partheight +5 + partdepth]} stroke="red" dash={[5,2]}></Line></Group>
+            return <Group><Line points={[dist,0,dist,partheight]} stroke="red" dash={[5,2]}></Line><Line points={[dist, -5,0,-5 - partdepth]} stroke="red" dash={[5,2]}></Line><Line points={[dist, partheight + 5,0,partheight + 5 + partdepth]} stroke="red" dash={[5,2]}></Line></Group>
         }
         else if(side === "right"){
-            return <Group><Line points={[partwidth - dist,0,partwidth - dist,partheight]} stroke="red" dash={[5,2]}></Line></Group>
+            return <Group><Line points={[partwidth - dist,0,partwidth - dist,partheight]} stroke="red" dash={[5,2]}></Line><Line points={[partwidth - dist, -5,partwidth,-5 - partdepth]} stroke="red" dash={[5,2]}></Line><Line points={[partwidth - dist, partheight + 5,partwidth,partheight + 5 + partdepth]} stroke="red" dash={[5,2]}></Line></Group>
         }
         else if(side === "top"){
-            return <Group><Line points={[0,dist,partwidth,dist]} stroke="red" dash={[5,2]}></Line></Group>
+            return <Group><Line points={[0,dist,partwidth,dist]} stroke="red" dash={[5,2]}></Line><Line points={[-5, dist,-5 - partdepth,0]} stroke="red" dash={[5,2]}></Line><Line points={[partwidth + 5, dist,partwidth + 5 + partdepth, 0]} stroke="red" dash={[5,2]}></Line></Group>
         }
         else{
-            return <Group><Line points={[0,partheight - dist,partwidth,partheight - dist]} stroke="red" dash={[5,2]}></Line></Group>
+            return <Group><Line points={[0,partheight - dist,partwidth,partheight - dist]} stroke="red" dash={[5,2]}></Line><Line points={[-5, partheight - dist,-5 - partdepth,partheight]} stroke="red" dash={[5,2]}></Line><Line points={[partwidth + 5, partheight - dist,partwidth + 5 + partdepth,partheight]} stroke="red" dash={[5,2]}></Line></Group>
         }
     }
 
