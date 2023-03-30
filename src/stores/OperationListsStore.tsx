51b403cb50d96ae9362eab95a40e852858826c3a
VisualX2 <visualx321@gmail.com>
2023-03-30 21:04:34 +0300

    Operation Lists update 1

---
+++ src/stores/OperationListsStore.tsx
@@ -0,0 +1,27 @@
+import { action, makeObservable, observable } from "mobx";
+import * as op from "../logic_stuff/OperationObjects"
+
+export class OperationListsStore {
+    @observable private drills: (op.Drill)[]
+
+    constructor(){
+        this.drills = []
+    }
+
+    @action getDrills() {
+        return this.drills
+    }
+
+    @action addDrill(x:number, y:number, depth:number, radius:number, side:string){
+        const d = new op.Drill(x,y,depth,radius,side)
+        this.drills.push(d)
+    }
+
+    @action removeDrill(id:string){
+        const indexOfObject = this.drills.findIndex(object => {
+            return object.id === id;
+        });
+        this.drills.splice(indexOfObject, 1);
+    } 
+        
+}
